be342cdbfc10789b5d098a462c97f9652214a469
Shane <obries47@tcd.ie>
2017-11-22 17:38:08 +0000

    Progress

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import Data.List
 import Network.Socket
 import System.IO
+import Data.List.Split
 import Control.Exception
 import Control.Concurrent
 import Control.Monad (when)
@@ -18,70 +19,66 @@ main = do
   bind sock (SockAddrInet 4242 iNADDR_ANY)
   -- listen :: socket -> MaxNumberOfQueueConnections
   listen sock 2
-  chan <- newChan
+  listeningChan <- newChan
+
+  -- this loop waits for connections, line 2 reads chan
   _ <- forkIO $ fix $ \loop -> do
-    (_, _) <- readChan chan
+    putStr "loop in main\n"
+    (_, _) <- readChan listeningChan
     loop
-  mainLoop sock chan 0
+  putStr "calling mainloop no idea why\n"
+  mainLoop sock listeningChan 0
 
 type Msg = (Int, String)
 
 mainLoop :: Socket -> Chan Msg -> Int -> IO ()
 mainLoop sock chan msgNum = do
+  putStr "loop in mainLoop\n"
   conn <- accept sock
   forkIO (runConn conn chan msgNum)
   -- $! = strict function application
   mainLoop sock chan $! msgNum + 1
 
-dropJust :: Maybe Int -> Int
-dropJust (Just a) = a
+-- offers lobby for the user to choose their chan
+runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO()
+runConn (sock, address) chan msgNum = do
+  hdl <- socketToHandle sock ReadWriteMode
+  hSetBuffering hdl NoBuffering
+  message <- fmap init (hGetLine hdl)
+  processMessage message address hdl
+
+processMessage :: String -> SockAddr -> Handle -> IO()
+processMessage msg address handle
+    | msg == "KILL_SERVICE\n" = putStr "Killing service"
+    | substring "HELO_text" msg = sendHeloText address handle
+    | substring "CHAT:" msg = putStr "Handling CHAT message"
+    | substring "DISCONNECT" msg = putStr "Disconnecting"
+    | substring "LEAVE_CHATROOM" msg = putStr "Joining chatroom"
+    | substring "JOIN_CHATROOM" msg = putStr "Joining chatroom"
+    | otherwise = putStr "ELSE I GUESS"
+
+substring :: String -> String -> Bool
+substring (x:xs) [] = False
+substring xs ys
+    | prefix xs ys = True
+    | substring xs (tail ys) = True
+    | otherwise = False
+
+prefix :: String -> String -> Bool
+prefix [] ys = True
+prefix (x:xs) [] = False
+prefix (x:xs) (y:ys) = (x == y) && prefix xs ys
+
+unpackJust :: Maybe Int -> Int
+unpackJust (Just a) = a
+unpackJust _  = 404
 
 getSockPort :: SockAddr -> String
-getSockPort x = tail $ drop (dropJust (elemIndex ':' $ show x)) $ show x
+getSockPort x = tail $ drop (unpackJust (elemIndex ':' $ show x)) $ show x
 
 getSockAddress :: SockAddr -> String
-getSockAddress x = take (dropJust (elemIndex ':' $ show x)) $ show x
+getSockAddress x = take (unpackJust (elemIndex ':' $ show x)) $ show x
 
 
-getHeloText :: SockAddr -> String
-getHeloText address = "HELO text\nIP: "  ++ getSockAddress address ++ "\n" ++ "Port: " ++ getSockPort address ++ "\nStudentID: 13324607\n"
-
-runConn :: (Socket, SockAddr) -> Chan Msg -> Int -> IO ()
-runConn (sock, address) chan msgNum = do
-    let broadcast msg = writeChan chan (msgNum, msg)
-    hdl <- socketToHandle sock ReadWriteMode
-    hSetBuffering hdl NoBuffering
-
-    hPutStrLn hdl "Hi, what's your name?"
-    name <- fmap init (hGetLine hdl)
-    broadcast ("--> " ++ name ++ " entered chat.")
-    hPutStrLn hdl ("Welcome, " ++ name ++ "!")
-
-
-
-    -- Duplicate a Chan: the duplicate channel begins empty, but data written to either channel from then on will be available from both.
-    -- Hence this creates a kind of broadcast channel, where data written by anyone is seen by everyone else.
-    commLine <- dupChan chan
-
-    -- fork off a thread for reading from the duplicated channel
-    reader <- forkIO $ fix $ \loop -> do
-      -- Read the next value from the Chan. Blocks when the channel is empty.
-      -- Since the read end of a channel is an MVar, this operation inherits fairness guarantees of MVars (e.g. threads blocked in this operation are woken up in FIFO order).
-        (nextNum, line) <- readChan commLine
-        when (msgNum /= nextNum) $ hPutStrLn hdl line
-        loop
-
-
-    handle (\(SomeException _) -> return ()) $ fix $ \loop -> do
-        line <- fmap init (hGetLine hdl)
-        case line of
-              -- "KILL_SERVICE" -> hPutStrLn hdl "Bye!"
-             -- If an exception is caught, send a message and break the loop
-             "KILL_SERVICE" -> hPutStrLn hdl "Bye!"
-             "HELO text" -> hPutStrLn hdl (getHeloText address) >> loop
-             -- else, continue looping.
-             _      -> broadcast (name ++ ": " ++ line) >> loop
-
-    killThread reader                      -- kill after the loop ends
-    broadcast ("<-- " ++ name ++ " left.") -- make a final broadcast
-    hClose hdl                  -- close handle
+sendHeloText :: SockAddr -> Handle -> IO()
+sendHeloText address hdl = hPutStr hdl ("HELO text\nIP: "  ++ getSockAddress address ++ "\n" ++ "Port: " ++ getSockPort address ++ "\nStudentID: 13324607\n")
