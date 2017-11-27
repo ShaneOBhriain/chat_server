77e0eb8dc244c074d2a9cd37514024fafc2a8529
Shane <obries47@tcd.ie>
2017-11-27 18:40:22 +0000

    Progress with sending chat

--- src/Main.hs
+++ src/Main.hs
@@ -23,26 +23,50 @@ main = do
   listeningChan <- newChan
 
   referenceList <- newIORef []
+  clientList <- newIORef []
 
   -- this loop waits for connections, line 2 reads chan
   _ <- forkIO $ fix $ \loop -> do
     (_, _) <- readChan listeningChan
     loop
 
-  mainLoop sock listeningChan 0 1 referenceList
+  mainLoop sock listeningChan 0 1 clientList referenceList
 
 type Msg = (Int, String)
 
 -- stringChan :: Chan Msg-> String
 -- stringChan (i s)= show i
 
-mainLoop :: Socket -> Chan Msg -> Int -> Int -> IORef [(Int, String, Chan Msg)]-> IO ()
-mainLoop sock chan msgNum clientNum chanListRef = do
+mainLoop :: Socket -> Chan Msg -> Int -> Int -> IORef [(Int, String)] -> IORef [(Int, String, Chan Msg)]-> IO ()
+mainLoop sock chan msgNum clientNum clientListRef chanListRef = do
   conn <- accept sock
   hdl <- setHandle conn
   -- line above waits until join then runs line below
-  forkIO (runConn conn hdl chan msgNum clientNum chanListRef)
-  mainLoop sock chan (msgNum+1) (clientNum + 1) chanListRef
+  forkIO (runConn conn hdl chan msgNum clientNum clientListRef chanListRef)
+  mainLoop sock chan (msgNum+1) (clientNum + 1) clientListRef chanListRef
+
+addClient :: String -> IORef [(Int, String)] -> IO ()
+addClient newClientName var = do
+    val <- readIORef var
+    let existingClient = getClientIdByName newClientName val
+    case existingClient of
+      Nothing -> do
+        let ref = length val + 1
+        let newVal = (ref, newClientName):val
+        writeIORef var newVal
+      Just x -> putStrLn "Already exists"
+
+getClientIdByName :: String -> [(Int,String)] -> Maybe Int
+getClientIdByName _ [] = Nothing
+getClientIdByName key ((a,b):ys)
+                  | key == b = Just a
+                  | otherwise = getClientIdByName key ys
+
+getClientNameById :: Int -> [(Int,String)] -> Maybe String
+getClientNameById _ [] = Nothing
+getClientNameById key ((a,b):ys)
+                  | key == a = Just b
+                  | otherwise = getClientNameById key ys
 
 setHandle :: (Socket,SockAddr) -> IO Handle
 setHandle (sock,addr) = do
@@ -77,26 +101,39 @@ getChanRefByString name (x:ys)
                   | name == getSecond x = getFirst x
                   | otherwise = getChanRefByString name ys
 
+sendMessage :: String -> String -> Int -> Chan Msg -> IO()
+sendMessage bigMessage clientName msgNum chan = do
+                          putStrLn "SENDING MESSAGE"
+                          let ref = getChatroomName bigMessage
+                          -- let clientName = "client name"
+                          let messageText = getClientName bigMessage
+                          let message = "CHAT: " ++ ref ++"\nCLIENT_NAME: " ++ clientName ++ "\nMESSAGE: " ++ messageText
+                          writeChan chan (msgNum, message)
+-- CHAT: [ROOM_REF]
+-- 		  CLIENT_NAME: [string identifying client user]
+-- 		  MESSAGE: [string terminated with '\n\n']
 
 
 -- offers lobby for the user to choose their chan
 -- Parameters: Socket, Handle of socket, Current Chan, List of Existing Chans, List of existing chan names
-runConn :: (Socket, SockAddr) -> Handle -> Chan Msg -> Int -> Int -> IORef [(Int, String, Chan Msg)] -> IO()
-runConn (sock, address) hdl chan msgNum clientNum chanListRef = do
-  let broadcast msg = writeChan chan (msgNum, msg)
+runConn :: (Socket, SockAddr) -> Handle -> Chan Msg -> Int -> Int -> IORef [(Int, String)] -> IORef [(Int, String, Chan Msg)] -> IO()
+runConn (sock, address) hdl chan msgNum clientNum clientListRef chanListRef = do
   message <- fmap init (hGetLine hdl)
   messageType <- processMessage message
   case messageType of
     0 -> hClose hdl
     1 -> sendHeloText address hdl
     2 -> do
-          broadcast message
+          clientList <- readIORef clientListRef
+          putStr $ "GETTING CLiENT NAME FROM ID: " ++ (show clientNum)
+          sendMessage message (unpackJustString $ getClientNameById clientNum clientList) msgNum chan
     3 -> do
           putStrLn "Disconnecting"
           hClose hdl
     4 -> do
           putStrLn "Joining chatroom"
           let clientName = getClientName message
+          addClient clientName clientListRef
           let chatroomName = getChatroomName message
           theChanList <- readIORef chanListRef
           let myNewChan = getChanByString chatroomName theChanList
@@ -108,32 +145,34 @@ runConn (sock, address) hdl chan msgNum clientNum chanListRef = do
                         theChanList <- readIORef chanListRef
                         hPutStrLn hdl $ printJoinedRoom chatroomName clientNum address theChanList
                         reader <- forkIO $ fix $ \loop -> do
+                            putStrLn "Reader loop"
                           -- Read the next value from the Chan. Blocks when the channel is empty.
                           -- Since the read end of a channel is an MVar, this operation inherits fairness guarantees of MVars (e.g. threads blocked in this operation are woken up in FIFO order).
                             (nextNum, line) <- readChan myChan
                             when (msgNum /= nextNum) $ hPutStrLn hdl line
                             loop
-                        runConn (sock, address) hdl myChan msgNum clientNum chanListRef
+                        runConn (sock, address) hdl myChan msgNum clientNum clientListRef chanListRef
             Just x -> do
                         myChan <- dupChan x
                         hPutStrLn hdl $ printJoinedRoom chatroomName clientNum address theChanList
                         reader <- forkIO $ fix $ \loop -> do
+                            putStrLn "Reader loop"
                           -- Read the next value from the Chan. Blocks when the channel is empty.
                           -- Since the read end of a channel is an MVar, this operation inherits fairness guarantees of MVars (e.g. threads blocked in this operation are woken up in FIFO order).
                             (nextNum, line) <- readChan myChan
                             when (msgNum /= nextNum) $ hPutStrLn hdl line
                             loop
-                        runConn (sock, address) hdl myChan msgNum clientNum chanListRef
+                        runConn (sock, address) hdl myChan msgNum clientNum clientListRef chanListRef
     5 -> do
           theChanList <- readIORef chanListRef
           hPutStrLn hdl $ printLeftRoom message clientNum
           myChan <- newChan
-          runConn (sock, address) hdl myChan msgNum clientNum chanListRef
+          runConn (sock, address) hdl myChan msgNum clientNum clientListRef chanListRef
     6 -> do
           putStrLn "broadcasting message"
-          broadcast message
+          runConn (sock, address) hdl chan msgNum clientNum clientListRef chanListRef
   putStrLn "Got to bottom"
-  runConn (sock, address) hdl chan msgNum clientNum chanListRef
+  runConn (sock, address) hdl chan msgNum clientNum clientListRef chanListRef
 
 printJoinedRoom :: String -> Int -> SockAddr -> [(Int, String, Chan Msg)]  -> String
 printJoinedRoom roomName clientNum address chanList = do
